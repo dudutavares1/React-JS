4cbc48f4ff0a432b9b6b2c88bb10c7e32f95055c
Dudu Tavares <skynetdudu@icloud.com>
2021-09-22 23:46:12 -0300

    instalando o react
    npm start

--- Formação React/01-JavaScript Orientado a Objetos/index.js
+++ Formação React/01-JavaScript Orientado a Objetos/index.js
@@ -1,51 +1,27 @@
-
-
-class Cliente {
-    nome;
-    cpf;
-
-
-}
-
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
-        }
-    }
-}
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
-cliente1.nome = "Dudu";
-cliente1.cpf = 34658877744;
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
 const cliente2 = new Cliente();
-cliente2.nome = "Mirella";
-cliente2.cpf = 34658445455;
-
-const contaCorrenteDudu = new ContaCorrente()
-contaCorrenteDudu.saldo = 0
-contaCorrenteDudu.agencia = 1001
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500)
 
 
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102
+console.log(conta2)
 
-
-
-console.log(contaCorrenteDudu.saldo)
-contaCorrenteDudu.saldo = 100
-console.log(contaCorrenteDudu.saldo)
-contaCorrenteDudu.sacar(50);
-
-
-
-
-
-//*
-console.log(cliente1)
-console.log(cliente2)
+contaCorrenteRicardo.transferir(200, conta2)
+console.log(conta2)
+console.log(contaCorrenteRicardo)
